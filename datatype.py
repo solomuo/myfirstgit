441a11202daf531a8a8c004c97c3009d9195726d
solomuo <mahoosolanki@gmail.com>
2019-07-04 23:24:15 -0700

    data type programm

---
+++ datatype.py
@@ -0,0 +1,67 @@
+# python data types
+
+
+
+
+#number
+
+#string
+
+#list
+
+#touple
+
+#dictionary
+
+#complex
+
+#number data type
+
+a = 10
+
+
+print(a)
+
+#output :10
+
+#string data type
+
+var = 'solomuo'
+
+print(var)
+
+#output: solomou
+
+#list data type
+
+mylist = [1,'solo',5,'ruby',6]
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+
+
+#Tuple data type
+
+mydata = ('dog','cat','parrot','dove','pig')
+
+print(mydata)
+
+# dictionary data type
+
+name_age = {'seema':20, 'ruby':18, 'rima':19}
+
+print(name_age['rime'])
+print(name_age['seema'])
+print(name_age['ruby'])
+
+
+# complex data type
+
+com = 12j
+print(com)
+
+#in python possible to will giving values with character
+
