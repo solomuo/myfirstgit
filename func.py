54f39467e382fed556d29760cc92d927f5af1555
solomuo <mahoosolanki@gmail.com>
2019-07-08 23:45:31 -0700

    function in python

---
+++ func.py
@@ -0,0 +1,20 @@
+
+#how create function in python
+
+""" Syntax of function
+
+    here "def" for function
+	
+	def function name(argument):
+	    #Stastement
+
+"""
+
+# function example
+
+def add(n1,n2):
+   print("Addition"+n1+n2)
+
+
+
+add(5,10)
